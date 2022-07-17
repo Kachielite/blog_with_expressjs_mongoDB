2b618a72bac81dc99b17d454d4bd1e6070d04b99
Kachielite <40024356+Kachielite@users.noreply.github.com>
2022-07-17 16:18:24 +0100

    fix filepath bug making delete blog post to fail

--- utils/deleteFile.js
+++ utils/deleteFile.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path')
 
 const deleteFile = (file) =>{
-    const filePath =  path.join('../',__dirname, file)
+    const filePath =  path.join('/app', file)
     fs.unlink(filePath, err =>{
        if(err){
         throw err;
